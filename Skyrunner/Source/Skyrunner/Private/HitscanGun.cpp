a82666af7d384d2f9dacbdcd3223fb5665c890ab
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-08-16 17:03:23 +0200

    add HitScanGun files

---
+++ Skyrunner/Source/Skyrunner/Private/HitscanGun.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitscanGun.h"
+
+void AHitscanGun::Fire(const FVector3d& dir)
+{
+	// GetWorld()->Raycas
+}
