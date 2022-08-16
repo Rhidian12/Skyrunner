44b33a44b18e68e8b8dc9a9f79dac6a63c0f83a6
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-08-16 17:16:47 +0200

    make hitscan fire

--- Skyrunner/Source/Skyrunner/Private/HitscanGun.cpp
+++ Skyrunner/Source/Skyrunner/Private/HitscanGun.cpp
@@ -5,5 +5,18 @@
 
 void AHitscanGun::Fire(const FVector3d& dir)
 {
-	// GetWorld()->Raycas
+	FHitResult hit{};
+
+	if (GetWorld()->LineTraceSingleByChannel(
+		hit,
+		StartLocation,
+		StartLocation + dir * 5000.f,
+		ECC_GameTraceChannel1))
+	{
+		DrawDebugLine(GetWorld(), StartLocation, hit.GetActor()->GetActorLocation(), FColor::Cyan, false, 5.f, 0u, 3.f);
+	}
+	else
+	{
+		DrawDebugLine(GetWorld(), StartLocation, StartLocation + dir * 5000.f, FColor::Cyan, false, 5.f, 0u, 3.f);
+	}
 }
