e56a534e1a07a0fe7008407f5ab4f4a554d96bcb
Daphné-Marie Huyghe <Daphnemarie.huyghe@gmail.com>
2022-08-16 17:17:11 +0200

    Added BP folder and PlayerControllers

---
+++ Skyrunner/Source/Skyrunner/Public/SkyrunnerPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SkyrunnerPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYRUNNER_API ASkyrunnerPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
