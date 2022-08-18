ed1e4605c82fd0f547659d9cf1124cb9040e6d71
Daphné-Marie Huyghe <Daphnemarie.huyghe@gmail.com>
2022-08-18 14:06:20 +0200

    Dashing Implemented
    Only thing which is still iffy is stopping the dash when a wall is hit at an angle, might not really be an issue though??

--- Skyrunner/Source/Skyrunner/Private/PlayerCharacter.cpp
+++ Skyrunner/Source/Skyrunner/Private/PlayerCharacter.cpp
@@ -2,7 +2,7 @@
 
 
 #include "PlayerCharacter.h"
-#include "GameFramework/PawnMovementComponent.h"
+#include "../SkyrunnerGameModeBase.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
@@ -17,6 +17,14 @@ void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	FScriptDelegate initializeDelegate{};
+	initializeDelegate.BindUFunction(this, "Init");
+	Cast<ASkyrunnerGameModeBase>(GetWorld()->GetAuthGameMode())->GetInitSystem()->OnGameStart.Add(initializeDelegate);
+}
+
+void APlayerCharacter::Init()
+{
+	pCharacterMovementComponent = Cast<UCharacterMovementComponent>(GetMovementComponent());
 }
 
 void APlayerCharacter::Dash()
@@ -24,22 +32,47 @@ void APlayerCharacter::Dash()
 	if (CanDash())
 	{
 		FVector playerForward{ GetActorForwardVector() };
+
+		playerForward.Z = 0;
+		bool successfulNormalise = playerForward.Normalize();
 		
-		if (GetMovementComponent()->IsMovingOnGround() && playerForward.Z < 0.0f)
+		if (successfulNormalise)
 		{
-			playerForward.Z = 0;
-			playerForward.Normalize();
+			OldGroundFriction = pCharacterMovementComponent->GroundFriction;
+			pCharacterMovementComponent->GroundFriction = pCharacterMovementComponent->FallingLateralFriction;
+			MovementSpeedPreDashSquared = pCharacterMovementComponent->Velocity.SquaredLength();
+
+			LaunchCharacter(playerForward * DashForce, true, true);
+			DashCooldownTimer = DashCooldownTime;
+			DashDurationTimer = DashDuration;
+			DashInvulnerabilityTimer = DashInvulnerabilityTime;
 		}
-		
-		LaunchCharacter(playerForward * DashForce, true, true);
-		DashCooldownTimer = DashCooldownTime;
-		DashInvulnerabilityTimer = DashInvulnerabilityTime;
 	}
+}
+
+void APlayerCharacter::OnDashEnd()
+{
+	if (!IsDashing()) // if we ended our dash at the end of it's duration (if we end early then it's because a sudden velocity change we don't wanna mess with)
+	{
+		// reset speed to speed before we dashed
+		// (pCharacterMovementComponent->Velocity.Normalize() * sqrt(MovementSpeedPreDashSquared))
+		pCharacterMovementComponent->Velocity.Normalize();
+		pCharacterMovementComponent->Velocity *= pCharacterMovementComponent->GetMaxSpeed();
+	}
+	pCharacterMovementComponent->GroundFriction = OldGroundFriction;
+
+	DashDurationTimer = 0; // reset timer in case we ended our dash by losing speed (Slamming into a wall)
+	DashInvulnerabilityTimer = 0; // reset timer in case we ended our dash by losing speed (Slamming into a wall)
 }
 
 bool APlayerCharacter::CanDash() const
 {
 	return DashCooldownTimer <= 0.0f;
+}
+
+bool APlayerCharacter::IsDashing() const
+{
+	return DashDurationTimer > 0.0f;
 }
 
 bool APlayerCharacter::IsInvulnerable() const
@@ -56,6 +89,14 @@ void APlayerCharacter::Tick(float DeltaTime)
 	if (!CanDash())
 	{
 		DashCooldownTimer -= DeltaTime;
+	}
+	if (IsDashing())
+	{
+		DashDurationTimer -= DeltaTime;
+		if (!IsDashing() || pCharacterMovementComponent->Velocity.SquaredLength() <= MovementSpeedPreDashSquared)
+		{
+			OnDashEnd();
+		}
 	}
 	if (IsInvulnerable())
 	{
