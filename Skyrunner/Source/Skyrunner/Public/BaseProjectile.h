c3093fc8de60a0a4fd39a629863f4c071d4e8adb
Rhidian De Wit <rhidiandewit@gmail.com>
2022-08-20 20:36:34 +0200

    add projectile gun and bullet

---
+++ Skyrunner/Source/Skyrunner/Public/BaseProjectile.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseProjectile.generated.h"
+
+UCLASS()
+class SKYRUNNER_API ABaseProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseProjectile();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Damage;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
---
+++ Skyrunner/Source/Skyrunner/Public/ProjectileGun.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseGun.h"
+#include "ProjectileGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYRUNNER_API AProjectileGun : public ABaseGun
+{
+	GENERATED_BODY()
+	
+};
